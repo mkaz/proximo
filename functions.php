8a7a7a463f7df63dd2543f47f8cc2987768b570e
Marcus Kazmierczak <marcus@mkaz.com>
2018-03-13 06:28:24 -0700

    Move disabling functionality to wpscrub plugin

--- functions.php
+++ functions.php
@@ -114,29 +114,3 @@ add_action( 'wp_enqueue_scripts', 'proximo_scripts' );
 
 require get_template_directory() . '/inc/template-tags.php';
 
-
-// disable emoji
-add_action( 'init', function() {
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	//add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
-} );
-
-// remove jetpack devicepx
-function remove_devicepx() {
-    wp_dequeue_script( 'devicepx' );
-}
-add_action( 'wp_enqueue_scripts', 'remove_devicepx', 20);
-
-
-// remove jquery-migrate
-add_action( 'wp_default_scripts', function( $scripts ) {
-    if ( ! empty( $scripts->registered['jquery'] ) ) {
-        $scripts->registered['jquery']->deps = array_diff( $scripts->registered['jquery']->deps, array( 'jquery-migrate' ) );
-    }
-} );
