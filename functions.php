bdd1dc70852278ddffa0ab579774d33a7470fd1e
Marcus Kazmierczak <marcus@mkaz.com>
2018-11-06 08:42:41 -0800

    Switch to local Charter font

--- functions.php
+++ functions.php
@@ -105,13 +105,7 @@ add_action( 'widgets_init', 'proximo_widgets_init' );
  */
 function proximo_scripts() {
 	wp_enqueue_style( 'proximo-style', get_stylesheet_uri(), filemtime( get_template_directory() . '/style.css' ) );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-
-	wp_register_style( 'googleFonts', '//fonts.googleapis.com/css?family=Roboto+Slab:400,700' );
-	wp_enqueue_style( 'googleFonts' );
+	wp_enqueue_style( 'proximo-fonts', get_template_directory_uri() . '/webfonts/stylesheet.css', filemtime( get_template_directory() . '/webfonts/stylesheet.css' ) );
 }
 add_action( 'wp_enqueue_scripts', 'proximo_scripts' );
 
