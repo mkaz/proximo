164ab8d3cc16eefaa44d6be149945826da8df3d7
Marcus Kazmierczak <marcus@mkaz.com>
2018-11-21 06:52:00 -0800

    Switch to arrays to simplify expanding fields/colors

---
+++ inc/customizer-colors.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Proximo: Customizer Colors
+ *
+ * Globo Arrays for colors used in customizer
+ * Maybe could be changed to functions instead of globals
+ *
+ * @package proximo
+ * @since 1.1.0
+ */
+
+$proximo_color_fields = array(
+    'bg_color' => 'Background',
+    'text_color' => 'Text',
+    'link_color' => 'Links',
+    'header_background' => 'Header Background',
+    'header_text_color' => 'Header Text'
+);
+
+$proximo_color_choices = array (
+    'gruvvy' => array(
+        'label' => 'Gruvvy',
+        'colors' => array(
+            'bg_color' => '#F5F5F5',
+            'text_color' => '#404040',
+            'link_color' => '#005DAC',
+            'header_background' => '#282828',
+            'header_text_color' => '#FE8019',
+        )
+    ),
+    'chino' => array(
+        'label' => 'Chino',
+        'colors' => array(
+            'bg_color' => '#EAD2AC',
+            'text_color' => '#404040',
+            'link_color' => '#9CAFB7',
+            'header_background' => '#4281A4',
+            'header_text_color' => '#FEF3FC'
+        )
+    ),
+    'bauhaus' => array(
+        'label' => 'Bauhaus',
+        'colors' => array(
+            'bg_color' => '#6AACC2',
+            'text_color' => '#648173',
+            'link_color' => '#C77121',
+            'header_background' => '#FCDC08',
+            'header_text_color' => '#648173'
+        )
+    ),
+    'marly' => array(
+        'label' => 'Marly',
+        'colors' => array(
+            'bg_color' => '#987EBC',
+            'text_color' => '#333333',
+            'link_color' => '#E678AC',
+            'header_background' => '#07084C',
+            'header_text_color' => '#ED9BD3'
+        )
+    ),
+    'maroon' => array(
+        'label' => 'Maroon',
+        'colors' => array(
+            'bg_color' => '#E3E2DF',
+            'text_color' => '#5D001E',
+            'link_color' => '#FC4445',
+            'header_background' => '#5D001E',
+            'header_text_color' => '#9A1750'
+        )
+    ),
+    'slate' => array(
+        'label' => 'Slate',
+        'colors' => array(
+            'bg_color' => '#8DA6CE',
+            'text_color' => '#333333',
+            'link_color' => '#FF6400',
+            'header_background' => '#0F152B',
+            'header_text' => '#FBDE2D'
+        )
+    ),
+    'candy' => array(
+        'label' => 'Candy',
+        'colors' => array(
+            'bg_color' => '#FF3A83',
+            'text_color' => '#333333',
+            'link_color' => '#EB36BD',
+            'header_background' => '#D785F7',
+            'header_text_color' => '#FFFFFF'
+        )
+    ),
+    'cafe' => array(
+        'label' => 'Cafe',
+        'colors' => array(
+            'bg_color' => '#8F7E65',
+            'text_color' => '#89360A',
+            'link_color' => '#89360A',
+            'header_background' => '#4B3516',
+            'header_text_color' => '#CBAD25'
+        )
+    ),
+);
--- inc/customizer.php
+++ inc/customizer.php
@@ -8,46 +8,10 @@
  * @since 1.1.0
  */
 
-/**
- * Colors order: Background, Text, Link, Header Background, Header Text
- */
-$proximo_color_choices = array (
-    'gruvvy' => array(
-        'label' => 'Gruvvy',
-        'colors' => array( '#F5F5F5', '#404040', '#005DAC', '#282828', '#FE8019' )
-    ),
-    'chino' => array(
-        'label' => 'Chino',
-        'colors' => array( '#EAD2AC', '#404040', '#9CAFB7', '#4281A4', '#FEF3FC' )
-    ),
-    'nelly' => array(
-        'label' => 'Nelly',
-        'colors' => array( '#FFA5A5', '#443344', '#EC368D', '#440381', '#51E5FF' )
-    ),
-    'coral' => array(
-        'label' => 'Coral',
-        'colors' => array( '#3FEEE6', '#333333', '#FC4445', '#FC4445', '#111111' )
-    ),
-    'maroon' => array(
-        'label' => 'Maroon',
-        'colors' => array( '#E3E2DF', '#5D001E', '#FC4445', '#5D001E', '#9A1750' )
-    ),
-    'slate' => array(
-        'label' => 'Slate',
-        'colors' => array( '#8DA6CE', '#333333', '#FF6400', '#0F152B', '#FBDE2D' )
-    ),
-    'candy' => array(
-        'label' => 'Candy',
-        'colors' => array( '#FF3A83', '#333333', '#EB36BD', '#D785F7', '#FFFFFF' )
-    ),
-    'cafe' => array(
-        'label' => 'Cafe',
-        'colors' => array( '#8F7E65', '#89360A', '#89360A', '#4B3516', '#CBAD25' )
-    ),
-);
+require( __DIR__ . "/customizer-colors.php" );
 
 function proximo_customize_register( $wp_customize ) {
-    global $proximo_color_choices;
+    global $proximo_color_choices, $proximo_color_fields;
 
     $wp_customize->add_setting( 'proximo_color_palette', array(
         'default' => 'gruvvy',
@@ -65,78 +29,30 @@ function proximo_customize_register( $wp_customize ) {
         'settings' => 'proximo_color_palette',
     ) ) );
 
-    // Background color
-    $wp_customize->add_setting( 'bg_color', array(
-        'default'   => $proximo_color_choices['gruvvy']['colors'][0],
-        'transport' => 'refresh',
-    ) );
+    foreach ( $proximo_color_fields as $field => $desc ) {
+        // Background color
+        $wp_customize->add_setting( $field, array(
+            'default'   => $proximo_color_choices['gruvvy']['colors'][$field],
+            'transport' => 'refresh',
+        ) );
 
-    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'bg_color', array(
-        'section' => 'colors',
-        'label'   => esc_html__( 'Background', 'proximo' ),
-    ) ) );
-
-    // Text color
-    $wp_customize->add_setting( 'text_color', array(
-        'default'   => $proximo_color_choices['gruvvy']['colors'][1],
-        'transport' => 'refresh',
-        'sanitize_callback' => 'sanitize_hex_color',
-    ) );
-
-    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'text_color', array(
-        'section' => 'colors',
-        'label'   => esc_html__( 'Text color', 'proximo' ),
-    ) ) );
-
-    // Link color
-    $wp_customize->add_setting( 'link_color', array(
-        'default'   => $proximo_color_choices['gruvvy']['colors'][2],
-        'transport' => 'refresh',
-        'sanitize_callback' => 'sanitize_hex_color',
-    ) );
-
-    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_color', array(
-        'section' => 'colors',
-        'label'   => esc_html__( 'Link color', 'proximo' ),
-    ) ) );
-
-    // Header background
-    $wp_customize->add_setting( 'header_background', array(
-        'default'   => $proximo_color_choices['gruvvy']['colors'][3],
-        'transport' => 'refresh',
-        'sanitize_callback' => 'sanitize_hex_color',
-    ) );
-
-    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_background', array(
-        'section' => 'colors',
-        'label'   => esc_html__( 'Header Background', 'proximo' ),
-    ) ) );
-
-    // Header Colors
-    $wp_customize->add_setting( 'header_text_color', array(
-        'default'   => $proximo_color_choices['gruvvy']['colors'][4],
-        'transport' => 'refresh',
-        'sanitize_callback' => 'sanitize_hex_color',
-    ) );
-
-    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_text_color', array(
-        'section' => 'colors',
-        'label'   => esc_html__( 'Header Text Color', 'proximo' ),
-    ) ) );
+        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, $field, array(
+            'section' => 'colors',
+            'label'   => esc_html__( $desc, 'proximo' ),
+        ) ) );
+    }
 }
 add_action( 'customize_register', 'proximo_customize_register' );
 
 function proximo_color_palette_callback( $color ) {
-    global $proximo_color_choices;
+    global $proximo_color_choices, $proximo_color_fields;
     if ( ! array_key_exists( $color, $proximo_color_choices ) ) {
         return $color;
     }
     $colors = $proximo_color_choices[ $color ]['colors'];
-    set_theme_mod( 'bg_color', $colors[0] );
-    set_theme_mod( 'text_color', $colors[1] );
-    set_theme_mod( 'link_color', $colors[2] );
-    set_theme_mod( 'header_background', $colors[3] );
-    set_theme_mod( 'header_text_color', $colors[4] );
+    foreach ( array_keys( $proximo_color_fields ) as $field ) {
+        set_theme_mod( $field, $colors[$field] );
+    }
 
     return $color;
 }
